1a007aaae2e9bb1587357cb6ccd59aec8e2768ff
Pinto Manuel <pintoydc@gmail.com>
2023-11-08 12:23:34 +0200

    typeScript added within the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-blue-500 min-h-screen flex justify-center items-center">
+      <header>
+        <h3 className="text-white text-center text-2xl font-bold">
+          Pinto Manuel
+        </h3>
+        <h1 className="text-white text-3xl font-bold">
+          SOLINK ASSESSMENT
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
